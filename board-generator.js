cf86c1045654e78cb1fbda2ae4cc4250bfd17197
Lucas Grinspan <lucasgrinspan@gmail.com>
2018-05-26 11:42:30 -0400

    Added coordinate system to css tiles

---
+++ board-generator.js
@@ -0,0 +1,24 @@
+var pieces = document.getElementsByClassName("piece");
+var board =    [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']];
+//  Reads board from application into array
+for (var i = 0; i < pieces.length; i++) {
+    var tileNumber = parseInt(pieces[i].parentElement.id.slice(5));
+    var pieceIcon = pieces[i].id;
+    board[Math.floor(tileNumber / 8)][tileNumber % 8] = pieceIcon;
+}
+console.log(board);
+for (var i = 0; i < 8; i++) {
+    var boardString = "";
+    for (var j = 0; j < 8; j++) {
+        boardString += (board[i][j] + " ");
+    }
+    console.log(boardString);
+    boardString = "";
+}
--- cpp_code/Evaluator.h
+++ cpp_code/Evaluator.h
@@ -4,6 +4,8 @@
 #include<vector>
 #include"Board.h"
 
+//  This class analyses the board and returns a numerical values from -10 to 10
+//  0 means neutral, 10 means white advantage, -10 means black advantage
 class Evaluator {
     public:
         Evaluator(std::vector<std::string> pieces);
